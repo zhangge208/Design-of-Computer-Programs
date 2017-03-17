8ff246e064f40d5a471b52d5da7d613e593ed65f
zhangge208 <zhangge208@gmail.com>
2017-03-17 23:41:44 +0800

    regular expression matching
    regular expression matching

---
+++ Lesson3/re.py
@@ -0,0 +1,8 @@
+def search(pattern, text):
+	"Return True if pattern appears anywhere in the text"
+
+def match(pattern, text):
+	"Return True if pattern appears at the start of text"
+
+def test():
+	
